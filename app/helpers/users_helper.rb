bd255d1703b87bf8eec5ce1c4065ce68f7052af3
sadmafioso <ivan.ramanenka@gmail.com>
2015-11-12 15:33:32 +0300

    fixed titles, added current balance, fixed ui

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+
+  def current_user_balance
+
+    moneyrecords = Moneyrecord.where(category_id: current_user.categories {|i| i.id})
+    sum_income = 0
+    sum_expense = 0
+
+    moneyrecords.each do |moneyrecord|
+      sum_income += moneyrecord.amount if moneyrecord.category.record_type == 1 and !moneyrecord.amount.nil?
+      sum_expense += moneyrecord.amount if moneyrecord.category.record_type == 0 and !moneyrecord.amount.nil?
+
+    end
+
+    total = sum_income-sum_expense
+
+  end
+
 end
